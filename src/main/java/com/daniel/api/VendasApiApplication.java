196f5cbe9960b485d0ff63ed2d2715edfa61d9d6
danielVFS <danielv759153@gmail.com>
2021-03-05 15:37:55 -0300

    Automatic seed database

--- src/main/java/com/daniel/api/VendasApiApplication.java
+++ src/main/java/com/daniel/api/VendasApiApplication.java
@@ -1,13 +1,31 @@
 package com.daniel.api;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.daniel.api.domain.Category;
+import com.daniel.api.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class VendasApiApplication {
+public class VendasApiApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
