94bfcffba23b08a3ef01f6eedbff980839193230
danielVFS <danielv759153@gmail.com>
2021-03-05 21:35:54 -0300

    Seed the database

--- src/main/java/com/daniel/api/VendasApiApplication.java
+++ src/main/java/com/daniel/api/VendasApiApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.api.domain.Category;
+import com.daniel.api.domain.Product;
 import com.daniel.api.repositories.CategoryRepository;
+import com.daniel.api.repositories.ProductRepository;
 
 @SpringBootApplication
 public class VendasApiApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
@@ -25,7 +30,20 @@ public class VendasApiApplication implements CommandLineRunner {
 		Category cat1 = new Category(null, "Informática");
 		Category cat2 = new Category(null, "Escritório");
 
+		Product p1 = new Product(null, "Computador", 2000.00);
+		Product p2 = new Product(null, "Impressora", 800.00);
+		Product p3 = new Product(null, "Mouse", 80.00);
+		
+		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProducts().addAll(Arrays.asList(p2));
+
+		p1.getCategories().addAll(Arrays.asList(cat1));
+		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategories().addAll(Arrays.asList(cat1));
+
+
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
