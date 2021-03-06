c27c7a873b8a072ffbc4f803424cde318c965203
danielVFS <danielv759153@gmail.com>
2021-03-06 09:33:01 -0300

    City and State repository and seed the database

--- src/main/java/com/daniel/api/VendasApiApplication.java
+++ src/main/java/com/daniel/api/VendasApiApplication.java
@@ -8,18 +8,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.daniel.api.domain.Category;
+import com.daniel.api.domain.City;
 import com.daniel.api.domain.Product;
+import com.daniel.api.domain.State;
 import com.daniel.api.repositories.CategoryRepository;
+import com.daniel.api.repositories.CityRepository;
 import com.daniel.api.repositories.ProductRepository;
+import com.daniel.api.repositories.StateRepository;
 
 @SpringBootApplication
 public class VendasApiApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private CityRepository cityRepository;
+	@Autowired
+	private StateRepository stateRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApiApplication.class, args);
@@ -33,7 +40,7 @@ public class VendasApiApplication implements CommandLineRunner {
 		Product p1 = new Product(null, "Computador", 2000.00);
 		Product p2 = new Product(null, "Impressora", 800.00);
 		Product p3 = new Product(null, "Mouse", 80.00);
-		
+
 		cat1.getProducts().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProducts().addAll(Arrays.asList(p2));
 
@@ -41,9 +48,25 @@ public class VendasApiApplication implements CommandLineRunner {
 		p2.getCategories().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategories().addAll(Arrays.asList(cat1));
 
-
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		/*******************************************************/
+
+		State st1 = new State(null, "Minas Gerais");
+		State st2 = new State(null, "São Paulo");
+
+		City c1 = new City(null, "Uberlândia", st1);
+		City c2 = new City(null, "São Paulo", st2);
+		City c3 = new City(null, "Campinas", st2);
+
+		st1.getCities().addAll(Arrays.asList(c1));
+		st2.getCities().addAll(Arrays.asList(c2, c3));
+
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		/*******************************************************/
 	}
 
 }
---
+++ src/main/java/com/daniel/api/repositories/CityRepository.java
@@ -0,0 +1,11 @@
+package com.daniel.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daniel.api.domain.City;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Integer> {
+
+}
