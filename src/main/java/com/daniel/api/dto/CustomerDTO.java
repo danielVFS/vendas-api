96add97cc14f4902101ad8122354e90ca3831901
danielVFS <danielv759153@gmail.com>
2021-03-08 11:08:41 -0300

    PUT-DELETE-GET - Customers

---
+++ src/main/java/com/daniel/api/dto/CustomerDTO.java
@@ -0,0 +1,55 @@
+package com.daniel.api.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Email;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.daniel.api.domain.Customer;
+
+public class CustomerDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message = "Name is required")
+	@Length(min = 5, max = 80, message = "The length must be between 5 and 8 characters ")
+	private String name;
+	
+	@NotEmpty(message = "Email is required")
+	@Email(message = "Invalid E-mail")
+	private String email;
+	
+	public CustomerDTO() {
+		super();
+	}
+	
+	public CustomerDTO(Customer customer) {
+		id = customer.getId();
+		name = customer.getName();
+		email = customer.getEmail();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
--- src/main/java/com/daniel/api/services/CustomerService.java
+++ src/main/java/com/daniel/api/services/CustomerService.java
@@ -1,12 +1,19 @@
 package com.daniel.api.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.daniel.api.domain.Customer;
+import com.daniel.api.dto.CustomerDTO;
 import com.daniel.api.repositories.CustomerRepository;
+import com.daniel.api.services.exceptions.DataIntegrityException;
 import com.daniel.api.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +27,37 @@ public class CustomerService {
 
 		return customer.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado, ID:" + id + ", Tipo: " + Customer.class.getName()));
+	}
+
+	public List<Customer> findAll() {
+		List<Customer> customer = customerRepository.findAll();
+
+		return customer;
+	}
+
+	public Page<Customer> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return customerRepository.findAll(pageRequest);
+	}
+
+	public Customer update(Customer category) {
+		find(category.getId());
+
+		return customerRepository.save(category);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			customerRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("It is not possible to delete a Customer that has products");
+		}
+	}
+
+	public Customer fromDTO(CustomerDTO customerDto) {
+		return new Customer(customerDto.getId(), customerDto.getName(), customerDto.getEmail(), null, null	);
 	}
 
 }
