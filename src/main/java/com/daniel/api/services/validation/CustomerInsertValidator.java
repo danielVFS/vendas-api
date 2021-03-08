c2d47c2528d4088feb61761443ebb04a29fdb032
danielVFS <danielv759153@gmail.com>
2021-03-08 16:16:08 -0300

    Custom validation: email on create customer

--- src/main/java/com/daniel/api/services/validation/CustomerInsertValidator.java
+++ src/main/java/com/daniel/api/services/validation/CustomerInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.daniel.api.domain.Customer;
 import com.daniel.api.dto.CustomerNewDTO;
 import com.daniel.api.enums.TypeCustomer;
+import com.daniel.api.repositories.CustomerRepository;
 import com.daniel.api.resources.exception.FieldMessage;
 import com.daniel.api.services.validation.utils.BR;
 
 public class CustomerInsertValidator implements ConstraintValidator<CustomerInsert, CustomerNewDTO> {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
 	@Override
 	public void initialize(CustomerInsert ann) {
 	}
@@ -26,6 +34,11 @@ public class CustomerInsertValidator implements ConstraintValidator<CustomerInse
 		
 		if(customerDto.getType().equals(TypeCustomer.LEGALPERSON.getCod()) && !BR.isValidCNPJ(customerDto.getCpfOrCnpj())) {
 			list.add(new FieldMessage("cpfOrCnpj", "CPF is invalid"));
+		}
+		
+		Customer customerAux = customerRepository.findByEmail(customerDto.getEmail());
+		if(customerAux != null) {
+			list.add(new FieldMessage("email", "this e-mail already exists"));
 		}
 			
 		for (FieldMessage e : list) {
